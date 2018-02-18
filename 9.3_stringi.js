b7b34439f996a87949758f41d80348d5ae4b4a77
Michal Rodzen <michalrodzen1994@gmail.com>
2018-02-19 00:31:09 +0100

    Add .js file

---
+++ 9.3_stringi.js
@@ -0,0 +1,7 @@
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period',
+dinosaur= 'triceratops', 
+dinosaurUpperCase = dinosaur.toUpperCase(),
+newText= text.replace('Velociraptor', dinosaurUpperCase),
+halfText = newText.substr(0, 72);
+
+console.log(halfText);
